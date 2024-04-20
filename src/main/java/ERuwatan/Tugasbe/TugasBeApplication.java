fcb7a68b4ee7182d9296a08fe45013270d613a0b
vanioersa <vanvanramadhan@gmail.com>
2024-04-20 09:50:04 +0700

    repo BE baru

---
+++ src/main/java/ERuwatan/Tugasbe/TugasBeApplication.java
@@ -0,0 +1,13 @@
+package ERuwatan.Tugasbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ERuwatan/Tugasbe/TugasBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ERuwatan.Tugasbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugasBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
