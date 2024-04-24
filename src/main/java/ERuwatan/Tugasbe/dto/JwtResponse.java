d34c41a2d297648990019b97341d71b4df7581e8
vanioersa <vanvanramadhan@gmail.com>
2024-04-24 08:07:37 +0700

    memperbaiki fungsi login agar bisa menuju page masing masing sesuai role nya

--- src/main/java/ERuwatan/Tugasbe/dto/JwtResponse.java
+++ src/main/java/ERuwatan/Tugasbe/dto/JwtResponse.java
@@ -9,15 +9,23 @@ public class JwtResponse implements Serializable {
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwttoken;
     private final UserModel userData;
+    private final String redirectUrl;
 
-    public JwtResponse(String jwttoken, UserModel userData) {
+    public JwtResponse(String jwttoken, UserModel userData, String redirectUrl) {
         this.jwttoken = jwttoken;
         this.userData = userData;
+        this.redirectUrl = redirectUrl;
     }
 
-    public UserModel getUserData() {return userData; }
+    public UserModel getUserData() {
+        return userData;
+    }
 
     public String getToken() {
         return this.jwttoken;
+    }
+
+    public String getRedirectUrl() {
+        return redirectUrl;
     }
 }
