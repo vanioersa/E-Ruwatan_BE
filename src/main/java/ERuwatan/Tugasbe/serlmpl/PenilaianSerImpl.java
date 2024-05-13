7135a43e7505565fc0d6a58a1e84a36652a636d5
yuniarahmah <yuraama75@gmail.com>
2024-05-13 10:50:37 +0700

    update data

--- src/main/java/ERuwatan/Tugasbe/serlmpl/PenilaianSerImpl.java
+++ src/main/java/ERuwatan/Tugasbe/serlmpl/PenilaianSerImpl.java
@@ -1,4 +1,9 @@
 package ERuwatan.Tugasbe.serlmpl;
 
+import ERuwatan.Tugasbe.repository.PenilaianRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class PenilaianSerImpl {
+    @Autowired
+    private PenilaianRepo penilaianRepo;
 }
