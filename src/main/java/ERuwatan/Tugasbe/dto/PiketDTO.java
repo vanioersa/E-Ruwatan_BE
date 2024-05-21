e7f09b894c111e3384a40b71788d35e9456d1681
yuniarahmah <yuraama75@gmail.com>
2024-05-21 15:40:59 +0700

    update data

--- src/main/java/ERuwatan/Tugasbe/dto/PiketDTO.java
+++ src/main/java/ERuwatan/Tugasbe/dto/PiketDTO.java
@@ -1,29 +1,12 @@
 package ERuwatan.Tugasbe.dto;
 
-import java.util.Date;
+import java.util.List;
 
 public class PiketDTO {
     private Long id;
     private Long kelasId;
-    private Long siswaId;
     private String tanggal;
-    private String status;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    private List<DpiketDTO> dpiketDTOList;
 
     public Long getKelasId() {
         return kelasId;
@@ -33,19 +16,30 @@ public class PiketDTO {
         this.kelasId = kelasId;
     }
 
-    public Long getSiswaId() {
-        return siswaId;
-    }
-
-    public void setSiswaId(Long siswaId) {
-        this.siswaId = siswaId;
-    }
-
     public String getTanggal() {
         return tanggal;
     }
 
     public void setTanggal(String tanggal) {
         this.tanggal = tanggal;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<DpiketDTO> getDpiketDTOList() {
+        return dpiketDTOList;
+    }
+
+    public void setDpiketDTOList(List<DpiketDTO> dpiketDTOList) {
+        this.dpiketDTOList = dpiketDTOList;
+    }
+
+    public void setSiswaId(Long aLong) {
     }
 }
