753a651c866a06809e46f0248d986524e419fe71
AndiSaputro50 <AndiSaputro50>
2024-05-28 08:20:53 +0700

    update error kelas repo

--- src/main/java/ERuwatan/Tugasbe/repository/KelasRepo.java
+++ src/main/java/ERuwatan/Tugasbe/repository/KelasRepo.java
@@ -1,11 +1,18 @@
 package ERuwatan.Tugasbe.repository;
 
 import ERuwatan.Tugasbe.dto.KelasDTO;
+import ERuwatan.Tugasbe.dto.SiswaDTO;
 import ERuwatan.Tugasbe.model.Kelas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface KelasRepo extends JpaRepository<Kelas, Long> {
 
-KelasDTO findByNamaKelas (String nama_kelas);
+//KelasDTO findByNamaKelas (String nama_kelas);
+
+    @Query(value = "SELECT * FROM siswa WHERE nama_kelas = :namaKelas" , nativeQuery = true)
+    Optional<KelasDTO> findByNamaKelas(String namaKelas);
 
 }
