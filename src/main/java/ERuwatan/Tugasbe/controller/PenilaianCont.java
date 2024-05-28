b141837d08d7f8a984f71edfba503084e9c7b660
yuniarahmah <yuraama75@gmail.com>
2024-05-28 15:22:39 +0700

    update data

--- src/main/java/ERuwatan/Tugasbe/controller/PenilaianCont.java
+++ src/main/java/ERuwatan/Tugasbe/controller/PenilaianCont.java
@@ -1,10 +1,13 @@
+
 package ERuwatan.Tugasbe.controller;
 
 import ERuwatan.Tugasbe.dto.PenilaianDTO;
 import ERuwatan.Tugasbe.serlmpl.PenilaianSerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,6 +20,7 @@ public class PenilaianCont {
     private PenilaianDTO createPenilaian (@RequestBody PenilaianDTO penilaianDTO) {
         return penilaianSer.createPenilaian(penilaianDTO);
     }
+
     @GetMapping("/by-id/{id}")
     public PenilaianDTO getPenilaianById(@PathVariable Long id) {
         return penilaianSer.getPenilaianById(id);
@@ -35,5 +39,10 @@ public class PenilaianCont {
     @DeleteMapping("/hapus/{id}")
     public void deletePenilaian(@PathVariable Long id) {
         penilaianSer.deletePenilaian(id);
+    }
+
+    @PostMapping("/import")
+    public void importPenilaianData(@RequestParam("file") MultipartFile file) throws IOException {
+        penilaianSer.importData(file);
     }
 }
--- src/main/java/ERuwatan/Tugasbe/service/PenilaianSer.java
+++ src/main/java/ERuwatan/Tugasbe/service/PenilaianSer.java
@@ -1,7 +1,9 @@
 package ERuwatan.Tugasbe.service;
 
 import ERuwatan.Tugasbe.dto.PenilaianDTO;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface PenilaianSer {
@@ -10,4 +12,5 @@ public interface PenilaianSer {
     List<PenilaianDTO> getAllPenilaian();
     PenilaianDTO updatePenilaian(Long id, PenilaianDTO penilaianDTO);
     void deletePenilaian(Long id);
+    void importData(MultipartFile file) throws IOException;
 }
