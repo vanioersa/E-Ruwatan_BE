59cba683c1bcd1f7251126f83cab89bf55aecc78
AndiSaputro50 <AndiSaputro50>
2024-06-12 07:26:36 +0700

    update import excel piket

--- src/main/java/ERuwatan/Tugasbe/Excell/ExcelPiket.java
+++ src/main/java/ERuwatan/Tugasbe/Excell/ExcelPiket.java
@@ -3,15 +3,12 @@ package ERuwatan.Tugasbe.Excell;
 import ERuwatan.Tugasbe.dto.KelasDTO;
 import ERuwatan.Tugasbe.dto.SiswaDTO;
 import ERuwatan.Tugasbe.model.Piket;
-import ERuwatan.Tugasbe.model.Siswa;
 import ERuwatan.Tugasbe.repository.KelasRepo;
 import ERuwatan.Tugasbe.repository.SiswaRepo;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityNotFoundException;
@@ -24,37 +21,28 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ExcelPiket {
     public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
     static String[] HEADERs = {"tanggal", "status", "kelas", "siswa"};
-
     static String SHEET = "Sheet1";
 
-    @Autowired
     private static KelasRepo kelasRepo;
-
-    public ExcelPiket(KelasRepo kelasRepo) {
-        this.kelasRepo = kelasRepo;
-    }
-
     private static SiswaRepo siswaRepo;
 
-    public ExcelPiket(SiswaRepo siswaRepo1) {
-        this.siswaRepo = siswaRepo1;
+    @Autowired
+    public ExcelPiket(KelasRepo kelasRepo, SiswaRepo siswaRepo) {
+        ExcelPiket.kelasRepo = kelasRepo;
+        ExcelPiket.siswaRepo = siswaRepo;
     }
 
     public static boolean hasExcelFormat(MultipartFile file) {
-        if (!TYPE.equals(file.getContentType())) {
-            return false;
-        }
-        return true;
+        return TYPE.equals(file.getContentType());
     }
 
-
-    public static ByteArrayInputStream pasienToExcel(List<Piket> pasiens) throws IOException {
+    public static ByteArrayInputStream piketToExcel(List<Piket> piketList) throws IOException {
         try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             Sheet sheet = workbook.createSheet(SHEET);
-
             Row headerRow = sheet.createRow(0);
 
             for (int col = 0; col < HEADERs.length; col++) {
@@ -62,21 +50,22 @@ public class ExcelPiket {
                 cell.setCellValue(HEADERs[col]);
             }
 
-
             int rowIdx = 1;
-            for (Piket pasien : pasiens) {
+            for (Piket piket : piketList) {
                 Row row = sheet.createRow(rowIdx++);
-                row.createCell(1).setCellValue(pasien.getTanggal());
-                row.createCell(2).setCellValue(pasien.getStatus());
-                row.createCell(3).setCellValue(pasien.getKelasId().getNama_kelas());
-                row.createCell(4).setCellValue(pasien.getSiswaId().getNama_siswa());
+                row.createCell(0).setCellValue(piket.getTanggal());
+                row.createCell(1).setCellValue(piket.getStatus());
+                row.createCell(2).setCellValue(piket.getKelasId().getNama_kelas());
+                row.createCell(3).setCellValue(piket.getSiswaId().getNama_siswa());
             }
+
             workbook.write(out);
             return new ByteArrayInputStream(out.toByteArray());
-        } catch(IOException e){
+        } catch (IOException e) {
             throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
         }
     }
+
     public static List<Piket> excelPiket(InputStream is) {
         try {
             Workbook workbook = new XSSFWorkbook(is);
@@ -101,13 +90,13 @@ public class ExcelPiket {
                     Cell currentCell = cellsInRow.next();
 
                     switch (cellIdx) {
-                        case 1:
+                        case 0:
                             piket.setTanggal(currentCell.getStringCellValue());
                             break;
-                        case 2:
+                        case 1:
                             piket.setStatus(currentCell.getStringCellValue());
                             break;
-                        case 3:
+                        case 2:
                             String kelasName = currentCell.getStringCellValue();
                             Optional<KelasDTO> optionalKelasDTO = kelasRepo.findByNamaKelas(kelasName);
                             if (optionalKelasDTO.isPresent()) {
@@ -117,14 +106,14 @@ public class ExcelPiket {
                                 throw new EntityNotFoundException("Kelas tidak ditemukan: " + kelasName);
                             }
                             break;
-                        case 4:
+                        case 3:
                             String siswaName = currentCell.getStringCellValue();
                             Optional<SiswaDTO> optionalSiswaDTO = siswaRepo.findBySiswa(siswaName);
                             if (optionalSiswaDTO.isPresent()) {
                                 SiswaDTO siswaDTO = optionalSiswaDTO.get();
                                 piket.setSiswaId(siswaDTO);
                             } else {
-                                throw new EntityNotFoundException("Siswa tidak di temukan: " + siswaName);
+                                throw new EntityNotFoundException("Siswa tidak ditemukan: " + siswaName);
                             }
                             break;
                         default:
@@ -141,5 +130,4 @@ public class ExcelPiket {
             throw new RuntimeException("Failed to parse Excel file: " + e.getMessage());
         }
     }
-
 }
